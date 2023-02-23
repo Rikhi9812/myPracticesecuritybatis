3c5017730275f4af8e3fa8a2735e5dc8668da197
Rikhi9812 <107738848+Rikhi9812@users.noreply.github.com>
2023-02-23 15:41:12 +0900

    1

--- src/test/java/com/example/demo/MyPracticesecuritybatisApplicationTests.java
+++ src/test/java/com/example/demo/MyPracticesecuritybatisApplicationTests.java
@@ -5,11 +5,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyPracticesecuritybatisApplicationTests {
-
-
-
+	
+	:alsjdvb a:lsj dv
+		]aslm v:asm 
+			sd v:asl v
+				:asm v:asm 
+					a:smc a:sm 
+						a:snc :as
+							
+	
 	@Test
 	void contextLoads() {
 	}
+
+	
+	/*
+a;sdjvbasjdbva@:jlsbcdv
+sljkdvbas:ljdbv
+:akjsc v:asjb
+a:ksncv d:askc
+a:slmc v:askmc
+
+*/
 
 }
