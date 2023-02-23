7533538d2884ca255b1b51fb0fb947f34e025612
Rikhi9812 <rishi.rana606@gmail.com>
2023-02-23 15:39:10 +0900

    test test jp1234

--- src/test/java/com/example/demo/MyPracticesecuritybatisApplicationTests.java
+++ src/test/java/com/example/demo/MyPracticesecuritybatisApplicationTests.java
@@ -6,27 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyPracticesecuritybatisApplicationTests {
 
+
+
 	@Test
 	void contextLoads() {
 	}
-	
-	SYtlasdkn:laskd
-		:lsdnva:lskdcvn
-			slajdv:alsjdk
-				ajsdvna:lskdv
-					alsdnv:als
-						a:lskdvn:asl
-							
-							
-							
-							
-							
-	Sysemoutlnsdlkn.prknt:alksnd
-		
-		void olkdnfa;lskdn(){
-			
-			test.nme;knas:;ldkn();
-		}
-							
 
 }
