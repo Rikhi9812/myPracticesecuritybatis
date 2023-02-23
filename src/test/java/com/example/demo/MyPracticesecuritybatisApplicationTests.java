919f3ac9a0e6c6f556dd05fe9e01eab51b7bcf1f
Rikhi9812 <rishi.rana606@gmail.com>
2023-02-23 15:49:39 +0900

    test  jpnew 1234

--- src/test/java/com/example/demo/MyPracticesecuritybatisApplicationTests.java
+++ src/test/java/com/example/demo/MyPracticesecuritybatisApplicationTests.java
@@ -6,71 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyPracticesecuritybatisApplicationTests {
 
-/*
-hello world
-		oajdh:aolsjkbdv
-	asldjvba:lsjdb
-	a;ksndcv a:sd
-	akmn cvas:km c
-*/
-
-
-
-	:alsjdvb a:lsj dv
-		]aslm v:asm
-			sd v:asl v
-				:asm v:asm
-					a:smc a:sm
-						a:snc :as
-
-
-
-							]alskdnva]lks
-							a]lsjdv c:aslkj
-								a:sj vas:km c
-									as;knc v;ask
-										a:ksnc a:sk
-
 	@Test
 	void contextLoads() {
 	}
-
-
-	/*
-hello world
-		oajdh:aolsjkbdv
-	asldjvba:lsjdb
-	a;ksndcv a:sd
-	akmn cvas:km c
-*/
-
-
-	/*
-hello world
-		oajdh:aolsjkbdv
-	asldjvba:lsjdb
-	a;ksndcv a:sd
-	akmn cvas:km c
-*/
-
-
-
-	/*
-a;sdjvbasjdbva@:jlsbcdv
-sljkdvbas:ljdbv
-:akjsc v:asjb
-a:ksncv d:askc
-a:slmc v:askmc
-
-*/
-
-	@naspdikvna
-
-	asjcbva@osjc
-		asjvbasjcb
-		asdjbva@bsd
-		aksjcv a@sjc
-		askjdbva@sdj
-
-
 }
