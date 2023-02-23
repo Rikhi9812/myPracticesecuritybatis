5e0f75502e74d22118ce91c71f7bcc2033a83985
Rikhi9812 <rishi.rana606@gmail.com>
2023-02-23 15:42:05 +0900

    test test jpnew 1

--- src/test/java/com/example/demo/MyPracticesecuritybatisApplicationTests.java
+++ src/test/java/com/example/demo/MyPracticesecuritybatisApplicationTests.java
@@ -6,10 +6,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyPracticesecuritybatisApplicationTests {
 
+/*
+hello world
+		oajdh:aolsjkbdv
+	asldjvba:lsjdb
+	a;ksndcv a:sd
+	akmn cvas:km c
+*/
 
 
 	@Test
 	void contextLoads() {
 	}
+
+
+	/*
+hello world
+		oajdh:aolsjkbdv
+	asldjvba:lsjdb
+	a;ksndcv a:sd
+	akmn cvas:km c
+*/
+
+
+	/*
+hello world
+		oajdh:aolsjkbdv
+	asldjvba:lsjdb
+	a;ksndcv a:sd
+	akmn cvas:km c
+*/
+
 
 }
